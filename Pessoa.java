2f3075fcb7c0d183a446dce87427d8bd7f9ab3d8
Everton L de Mattos Santana <106360254+Everton-Mattos@users.noreply.github.com>
2023-07-19 18:12:59 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,29 @@
+public class Pessoa {
+    private String nome;
+    private String sobrenome;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void sobrenome(String sobrenome) {
+        return sobrenome;
+    }
+
+    public void idade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    if(nome) {
+        system.out.println("Meu nome eh everton");
+    } else {
+        system.out.println("Não achei o nome");
+    }
+
+
+}
